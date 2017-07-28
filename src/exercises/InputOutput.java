4c43c40ef5a1328b67e6ac3a2b91f76f428f10aa
Michael Alarcon <malarconii@yahoo.com>
2017-07-27 22:03:46 -0700

    Added input/output to hello world

--- src/exercises/InputOutput.java
+++ src/exercises/InputOutput.java
@@ -1,9 +1,23 @@
 package exercises;
+import java.util.Scanner;
 
 public class InputOutput {
 
     public static void main(String[] args) {
 
-        
+        helloWorld();
+
+    }
+
+    public static void helloWorld() {
+
+        Scanner name;
+
+        name = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String inputName = name.next();
+
+        System.out.print("Hello " + inputName);
+
     }
 }
